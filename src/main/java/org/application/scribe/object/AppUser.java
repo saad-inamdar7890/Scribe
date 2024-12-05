49182af63a2a05921a80441eb62cb8c715f34fea
Saad Inamdar <saadsa27_soe@jnu.ac.in>
2024-12-05 20:38:23 +0530

    everything is done

--- src/main/java/org/application/scribe/object/AppUser.java
+++ src/main/java/org/application/scribe/object/AppUser.java
@@ -7,13 +7,15 @@ import jakarta.persistence.Id;
 import lombok.Data;
 import jakarta.persistence.Table;
 
+import java.math.BigInteger;
+
 @Entity
 @Data
 @Table(name = "\"Appuser\"")
 public class AppUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private int id;
     private String name;
     private String school;
     private String course;
--- src/main/java/org/application/scribe/object/Request.java
+++ src/main/java/org/application/scribe/object/Request.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.JoinColumn;
 import lombok.Data;
+
+import java.math.BigInteger;
 import java.time.LocalDateTime;
 
 @Entity
@@ -14,14 +16,14 @@ import java.time.LocalDateTime;
 public class Request {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id; // Unique identifier for the request
+    private int id; // Unique identifier for the request
     private LocalDateTime examDate; // Date and time of the exam
     private String examName; // Name of the exam
     private String examTime; // Time of the exam
     private String venue; // Venue of the exam
 
     @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false) // Foreign key to User
+    @JoinColumn(name = "appUser_id", nullable = false) // Foreign key to User
     private AppUser appUser_id; // Reference to the User object
 
 
